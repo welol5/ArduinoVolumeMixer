adf403d2019eb526e6fbd7af756284476ba0af5f
welol5 <41875147+welol5@users.noreply.github.com>
2018-07-29 14:41:22 -0400

    Add files via upload

---
+++ AudioSessionVolumeManagerCS/AudioSessionVolumeManagerCS/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace AudioSessionVolumeManagerCS
+{
+    partial class AudioSessionVolumeManagerCS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AudioSessionVolumeManagerCS));
+            this.MasterMuteButton = new System.Windows.Forms.Button();
+            this.MasterVolumeSlider = new System.Windows.Forms.TrackBar();
+            this.Session1Selector = new System.Windows.Forms.ComboBox();
+            this.Session1Volume = new System.Windows.Forms.TrackBar();
+            this.Session2Volume = new System.Windows.Forms.TrackBar();
+            this.Session2Selector = new System.Windows.Forms.ComboBox();
+            this.Session3Volume = new System.Windows.Forms.TrackBar();
+            this.Session3Selector = new System.Windows.Forms.ComboBox();
+            this.Session4Volume = new System.Windows.Forms.TrackBar();
+            this.Session4Selector = new System.Windows.Forms.ComboBox();
+            this.ComSelector = new System.Windows.Forms.ComboBox();
+            this.ComSelectText = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MasterVolumeSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Session1Volume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Session2Volume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Session3Volume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Session4Volume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MasterMuteButton
+            // 
+            this.MasterMuteButton.Location = new System.Drawing.Point(10, 34);
+            this.MasterMuteButton.Name = "MasterMuteButton";
+            this.MasterMuteButton.Size = new System.Drawing.Size(121, 23);
+            this.MasterMuteButton.TabIndex = 0;
+            this.MasterMuteButton.Text = "Master";
+            this.MasterMuteButton.UseVisualStyleBackColor = true;
+            this.MasterMuteButton.Click += new System.EventHandler(this.MasterMuteButton_Click);
+            // 
+            // MasterVolumeSlider
+            // 
+            this.MasterVolumeSlider.Location = new System.Drawing.Point(48, 63);
+            this.MasterVolumeSlider.Maximum = 1000;
+            this.MasterVolumeSlider.Name = "MasterVolumeSlider";
+            this.MasterVolumeSlider.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.MasterVolumeSlider.Size = new System.Drawing.Size(45, 367);
+            this.MasterVolumeSlider.TabIndex = 1;
+            this.MasterVolumeSlider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.MasterVolumeSlider.Scroll += new System.EventHandler(this.MasterVolumeSlider_Scroll);
+            // 
+            // Session1Selector
+            // 
+            this.Session1Selector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Session1Selector.FormattingEnabled = true;
+            this.Session1Selector.Location = new System.Drawing.Point(136, 34);
+            this.Session1Selector.Name = "Session1Selector";
+            this.Session1Selector.Size = new System.Drawing.Size(121, 21);
+            this.Session1Selector.TabIndex = 2;
+            this.Session1Selector.SelectedIndexChanged += new System.EventHandler(this.Session1Selector_SelectedIndexChanged);
+            // 
+            // Session1Volume
+            // 
+            this.Session1Volume.Location = new System.Drawing.Point(174, 61);
+            this.Session1Volume.Maximum = 1000;
+            this.Session1Volume.Name = "Session1Volume";
+            this.Session1Volume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.Session1Volume.Size = new System.Drawing.Size(45, 367);
+            this.Session1Volume.TabIndex = 3;
+            this.Session1Volume.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.Session1Volume.Scroll += new System.EventHandler(this.Session1Volume_Scroll);
+            // 
+            // Session2Volume
+            // 
+            this.Session2Volume.Location = new System.Drawing.Point(300, 61);
+            this.Session2Volume.Maximum = 1000;
+            this.Session2Volume.Name = "Session2Volume";
+            this.Session2Volume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.Session2Volume.Size = new System.Drawing.Size(45, 367);
+            this.Session2Volume.TabIndex = 5;
+            this.Session2Volume.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.Session2Volume.Scroll += new System.EventHandler(this.Session2Volume_Scroll);
+            // 
+            // Session2Selector
+            // 
+            this.Session2Selector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Session2Selector.FormattingEnabled = true;
+            this.Session2Selector.Location = new System.Drawing.Point(262, 34);
+            this.Session2Selector.Name = "Session2Selector";
+            this.Session2Selector.Size = new System.Drawing.Size(121, 21);
+            this.Session2Selector.TabIndex = 4;
+            this.Session2Selector.SelectedIndexChanged += new System.EventHandler(this.Session2Selector_SelectedIndexChanged);
+            // 
+            // Session3Volume
+            // 
+            this.Session3Volume.Location = new System.Drawing.Point(425, 61);
+            this.Session3Volume.Maximum = 1000;
+            this.Session3Volume.Name = "Session3Volume";
+            this.Session3Volume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.Session3Volume.Size = new System.Drawing.Size(45, 367);
+            this.Session3Volume.TabIndex = 7;
+            this.Session3Volume.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.Session3Volume.Scroll += new System.EventHandler(this.Session3Volume_Scroll);
+            // 
+            // Session3Selector
+            // 
+            this.Session3Selector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Session3Selector.FormattingEnabled = true;
+            this.Session3Selector.Location = new System.Drawing.Point(387, 34);
+            this.Session3Selector.Name = "Session3Selector";
+            this.Session3Selector.Size = new System.Drawing.Size(121, 21);
+            this.Session3Selector.TabIndex = 6;
+            this.Session3Selector.SelectedIndexChanged += new System.EventHandler(this.Session3Selector_SelectedIndexChanged);
+            // 
+            // Session4Volume
+            // 
+            this.Session4Volume.Location = new System.Drawing.Point(552, 61);
+            this.Session4Volume.Maximum = 1000;
+            this.Session4Volume.Name = "Session4Volume";
+            this.Session4Volume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.Session4Volume.Size = new System.Drawing.Size(45, 367);
+            this.Session4Volume.TabIndex = 9;
+            this.Session4Volume.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.Session4Volume.Scroll += new System.EventHandler(this.Session4Volume_Scroll);
+            // 
+            // Session4Selector
+            // 
+            this.Session4Selector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Session4Selector.FormattingEnabled = true;
+            this.Session4Selector.Location = new System.Drawing.Point(514, 34);
+            this.Session4Selector.Name = "Session4Selector";
+            this.Session4Selector.Size = new System.Drawing.Size(121, 21);
+            this.Session4Selector.TabIndex = 8;
+            this.Session4Selector.SelectedIndexChanged += new System.EventHandler(this.Session4Selector_SelectedIndexChanged);
+            // 
+            // ComSelector
+            // 
+            this.ComSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComSelector.FormattingEnabled = true;
+            this.ComSelector.Location = new System.Drawing.Point(70, 7);
+            this.ComSelector.Name = "ComSelector";
+            this.ComSelector.Size = new System.Drawing.Size(121, 21);
+            this.ComSelector.TabIndex = 11;
+            this.ComSelector.SelectedIndexChanged += new System.EventHandler(this.ComSelector_SelectedIndexChanged);
+            // 
+            // ComSelectText
+            // 
+            this.ComSelectText.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ComSelectText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ComSelectText.Location = new System.Drawing.Point(11, 10);
+            this.ComSelectText.Name = "ComSelectText";
+            this.ComSelectText.Size = new System.Drawing.Size(118, 13);
+            this.ComSelectText.TabIndex = 12;
+            this.ComSelectText.Tag = "";
+            this.ComSelectText.Text = "Com Port:";
+            // 
+            // AudioSessionVolumeManagerCS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 450);
+            this.Controls.Add(this.ComSelector);
+            this.Controls.Add(this.ComSelectText);
+            this.Controls.Add(this.Session4Volume);
+            this.Controls.Add(this.Session4Selector);
+            this.Controls.Add(this.Session3Volume);
+            this.Controls.Add(this.Session3Selector);
+            this.Controls.Add(this.Session2Volume);
+            this.Controls.Add(this.Session2Selector);
+            this.Controls.Add(this.Session1Volume);
+            this.Controls.Add(this.Session1Selector);
+            this.Controls.Add(this.MasterVolumeSlider);
+            this.Controls.Add(this.MasterMuteButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AudioSessionVolumeManagerCS";
+            this.Text = "AudioSessionVolumeManagerCS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AudioSessionVolumeManagerCS_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.MasterVolumeSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Session1Volume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Session2Volume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Session3Volume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Session4Volume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MasterMuteButton;
+        private System.Windows.Forms.TrackBar MasterVolumeSlider;
+        private System.Windows.Forms.ComboBox Session1Selector;
+        private System.Windows.Forms.TrackBar Session1Volume;
+        private System.Windows.Forms.TrackBar Session2Volume;
+        private System.Windows.Forms.ComboBox Session2Selector;
+        private System.Windows.Forms.TrackBar Session3Volume;
+        private System.Windows.Forms.ComboBox Session3Selector;
+        private System.Windows.Forms.TrackBar Session4Volume;
+        private System.Windows.Forms.ComboBox Session4Selector;
+        private System.Windows.Forms.ComboBox ComSelector;
+        private System.Windows.Forms.TextBox ComSelectText;
+    }
+}
+
---
+++ AudioSessionVolumeManagerCS/AudioSessionVolumeManagerCS/Form1.cs
@@ -0,0 +1,236 @@
+﻿/*TODO:
+ * USE VOLUMECHANGED EVENTS TO UPDATE SLIDERS
+ * MAKE VOLUME SESSION LIST UPDATE
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+using System.Text.RegularExpressions;
+
+using System.IO.Ports;
+using System.Threading;
+
+namespace AudioSessionVolumeManagerCS
+{
+
+    public partial class AudioSessionVolumeManagerCS : Form
+    {
+        //audio vars
+        MMDevice device;
+        AudioEndpointVolume masterVolume;
+        SessionCollection sessions;
+        int[] currentSelectedIndex = { -1, -1, -1, -1 };
+
+        //serial vars
+        SerialPort port = null;
+        Thread readThread;
+        private bool serialRead;
+        string[] levels;
+
+        public AudioSessionVolumeManagerCS()
+        {
+            InitializeComponent();
+
+            //intialize audio stuff
+            var deviceEnumerator = new MMDeviceEnumerator();
+            device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            masterVolume = device.AudioEndpointVolume;
+            updateMasterVolumeSlider();
+            masterVolume.OnVolumeNotification += endpointVolumeNotification;
+
+            //add the session volumes to the list
+            updateSessions();
+            device.AudioSessionManager.OnSessionCreated += sessionCreatedNotificaton;
+
+            //intialize serial stuff
+            string portName;
+            foreach(string s in SerialPort.GetPortNames())
+            {
+                //Debug.WriteLine(s);
+                ComSelector.Items.Add(s);
+            }
+            //readThread = new Thread(read);
+            //port = new SerialPort("COM7", 9600, Parity.None, 8, StopBits.One);
+            //port.Open();
+            //serialRead = true;
+            //readThread.Start();
+        }
+
+        //master volume
+        private void MasterMuteButton_Click(object sender, EventArgs e)
+        {
+            masterVolume.Mute = !masterVolume.Mute;
+        }
+        void endpointVolumeNotification(AudioVolumeNotificationData data)
+        {
+            try
+            {
+                this.Invoke(new Action(delegate ()
+                {
+                    updateMasterVolumeSlider();
+                }));
+            }
+            catch { }
+        }
+        void updateMasterVolumeSlider()
+        {
+            MasterVolumeSlider.Value = (int)(masterVolume.MasterVolumeLevelScalar * 1000);
+        }
+        private void MasterVolumeSlider_Scroll(object sender, EventArgs e)
+        {
+            try
+            {
+                masterVolume.MasterVolumeLevelScalar = (float)(MasterVolumeSlider.Value / 1000.0);
+            } catch { }
+        }
+
+        //program volumes
+        void updateSessions()
+        {
+            Session1Selector.Items.Clear();
+            Session2Selector.Items.Clear();
+            Session3Selector.Items.Clear();
+            Session4Selector.Items.Clear();
+
+            sessions = device.AudioSessionManager.Sessions;
+            for (int i = 0; i < sessions.Count; i++)
+            {
+                Session1Selector.Items.Add(Process.GetProcessById((int)(sessions[i].GetProcessID)).ProcessName);
+                Session2Selector.Items.Add(Process.GetProcessById((int)(sessions[i].GetProcessID)).ProcessName);
+                Session3Selector.Items.Add(Process.GetProcessById((int)(sessions[i].GetProcessID)).ProcessName);
+                Session4Selector.Items.Add(Process.GetProcessById((int)(sessions[i].GetProcessID)).ProcessName);
+            }
+        }
+        void sessionCreatedNotificaton(object sender, IAudioSessionControl newSession)
+        {
+            try
+            {
+                this.Invoke(new Action(delegate ()
+                {
+                    device.AudioSessionManager.RefreshSessions();
+                    updateSessions();
+                }));
+            } catch { }
+        }
+
+        //first session
+        private void Session1Selector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setSlider1Position();
+            currentSelectedIndex[0] = Session1Selector.SelectedIndex;
+        }
+        void setSlider1Position()
+        {
+            //Debug.WriteLine(Session1Selector.SelectedIndex);
+            Session1Volume.Value = (int)(sessions[Session1Selector.SelectedIndex].SimpleAudioVolume.Volume*1000);
+        }
+        private void Session1Volume_Scroll(object sender, EventArgs e)
+        {
+            sessions[Session1Selector.SelectedIndex].SimpleAudioVolume.Volume = (float)(Session1Volume.Value / 1000.0);
+        }
+
+        //second session
+        private void Session2Selector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setSlider2Position();
+            currentSelectedIndex[1] = Session2Selector.SelectedIndex;
+        }
+        void setSlider2Position()
+        {
+            Session2Volume.Value = (int)(sessions[Session2Selector.SelectedIndex].SimpleAudioVolume.Volume);
+        }
+        private void Session2Volume_Scroll(object sender, EventArgs e)
+        {
+            sessions[Session2Selector.SelectedIndex].SimpleAudioVolume.Volume = (float)(Session2Volume.Value / 1000.0);
+        }
+
+        //third session
+        private void Session3Selector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setSlider3Position();
+            currentSelectedIndex[2] = Session3Selector.SelectedIndex;
+        }
+        void setSlider3Position()
+        {
+            Session3Volume.Value = (int)(sessions[Session3Selector.SelectedIndex].SimpleAudioVolume.Volume);
+        }
+        private void Session3Volume_Scroll(object sender, EventArgs e)
+        {
+            sessions[Session3Selector.SelectedIndex].SimpleAudioVolume.Volume = (float)(Session3Volume.Value / 1000.0);
+        }
+
+        //fourth session
+        private void Session4Selector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setSlider4Position();
+            currentSelectedIndex[3] = Session4Selector.SelectedIndex;
+        }
+        void setSlider4Position()
+        {
+            Session4Volume.Value = (int)(sessions[Session4Selector.SelectedIndex].SimpleAudioVolume.Volume);
+        }
+        private void Session4Volume_Scroll(object sender, EventArgs e)
+        {
+            sessions[Session4Selector.SelectedIndex].SimpleAudioVolume.Volume = (float)(Session4Volume.Value / 1000.0);
+        }
+
+        //serial
+        public void read()
+        {
+            while (serialRead)
+            {
+                try
+                {
+                    string data = port.ReadLine();
+                    //Debug.WriteLine(data);
+                    levels = data.Split(',');
+                    /*foreach (string s in levels)
+                    {
+                        Debug.WriteLine(s);
+                    }*/
+
+                    //TODO: set the serial levels to the volume levels
+                    masterVolume.MasterVolumeLevelScalar = (float)(Int32.Parse(Regex.Match(levels[1], "\\d+").Value) / 100.0);
+                    if(currentSelectedIndex[0] != -1)
+                        sessions[currentSelectedIndex[0]].SimpleAudioVolume.Volume = (float)(Int32.Parse(Regex.Match(levels[2], "\\d+").Value) / 100.0);
+                    if (currentSelectedIndex[1] != -1)
+                        sessions[currentSelectedIndex[1]].SimpleAudioVolume.Volume = (float)(Int32.Parse(Regex.Match(levels[3], "\\d+").Value) / 100.0);
+                    if (currentSelectedIndex[2] != -1)
+                        sessions[currentSelectedIndex[2]].SimpleAudioVolume.Volume = (float)(Int32.Parse(Regex.Match(levels[4], "\\d+").Value) / 100.0);
+                    if (currentSelectedIndex[3] != -1)
+                        sessions[currentSelectedIndex[3]].SimpleAudioVolume.Volume = (float)(Int32.Parse(Regex.Match(levels[5], "\\d+").Value) / 100.0);
+                }
+                catch (TimeoutException) { }
+                catch (IndexOutOfRangeException) { }
+                catch { }
+            }
+        }
+        private void ComSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (port == null)
+            {
+                readThread = new Thread(read);
+                port = new SerialPort(ComSelector.SelectedItem.ToString(), 9600, Parity.None, 8, StopBits.One);
+                port.Open();
+                serialRead = true;
+                readThread.Start();
+            }
+        }
+
+        private void AudioSessionVolumeManagerCS_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialRead = false;
+        }
+    }
+}
